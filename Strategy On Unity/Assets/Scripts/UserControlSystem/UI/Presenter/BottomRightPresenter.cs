83f02179197a328b0658a2c7ccb63df104f9414f
Maks Sapunov <Budgo@mail.ru>
2022-04-16 19:33:14 +0300

    Задание 2

---
+++ Strategy On Unity/Assets/Scripts/UserControlSystem/UI/Presenter/BottomRightPresenter.cs
@@ -0,0 +1,50 @@
+﻿using Abstractions;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserControlSystem.UI.Presenter
+{
+    internal class BottomRightPresenter : MonoBehaviour
+    {
+        [SerializeField] private Button _moveButton;
+        [SerializeField] private Button _attackButton;
+        [SerializeField] private Button _patrolButton;
+        [SerializeField] private Button _holdPositionButton;
+
+        [SerializeField] private SelectableValue _selectedValue;
+
+        private void Start()
+        {
+            _selectedValue.OnSelected += ONSelected;
+            ONSelected(_selectedValue.CurrentValue);
+        }
+
+        private void ONSelected(ISelectable selected)
+        {
+            if (selected != null)
+            {
+                OnButtons();
+            }
+            else
+            {
+                OffButtons();
+            }
+        }
+
+        private void OnButtons()
+        {
+            _moveButton.gameObject.SetActive(true);
+            _attackButton.gameObject.SetActive(true);
+            _patrolButton.gameObject.SetActive(true);
+            _holdPositionButton.gameObject.SetActive(true);
+        }
+
+        private void OffButtons()
+        {
+            _moveButton.gameObject.SetActive(false);
+            _attackButton.gameObject.SetActive(false);
+            _patrolButton.gameObject.SetActive(false);
+            _holdPositionButton.gameObject.SetActive(false);
+        }
+    }
+}
