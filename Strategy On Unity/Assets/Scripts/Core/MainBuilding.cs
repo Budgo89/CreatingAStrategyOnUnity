a9ad7e737f7981a0fb10da36b06e8fbaa14f74aa
Maks Sapunov <Budgo@mail.ru>
2022-04-20 23:10:53 +0300

    Подготовка к ДЗ-2

--- Strategy On Unity/Assets/Scripts/Core/MainBuilding.cs
+++ Strategy On Unity/Assets/Scripts/Core/MainBuilding.cs
@@ -1,54 +1,24 @@
 using Abstractions;
-using Assets.Scripts.Core;
+using Abstractions.Commands;
+using Abstractions.Commands.CommandsInterfaces;
 using UnityEngine;
 
-namespace Core
+public sealed class MainBuilding : CommandExecutorBase<IProduceUnitCommand>, ISelectable
 {
-    public sealed class MainBuilding : MonoBehaviour, IUnitProducer, ISelectable
-    {
-        public float Health => _health;
-        public float MaxHealth => _maxHealth;
-        public Sprite Icon => _icon;
+    public float Health => _health;
+    public float MaxHealth => _maxHealth;
+    public Sprite Icon => _icon;
 
-        [SerializeField] private GameObject _unitPrefab;
-        [SerializeField] private Transform _unitsParent;
+    [SerializeField] private Transform _unitsParent;
 
-        [SerializeField] private float _maxHealth = 1000;
-        [SerializeField] private Sprite _icon;
-        [SerializeField] private GameObject _selection;
+    [SerializeField] private float _maxHealth = 1000;
+    [SerializeField] private Sprite _icon;
 
-        private float _health = 1000;
+    private float _health = 1000;
 
-        [SerializeField] private SelectableValueSelected _selectedValueSelected;
-
-        private void Start()
-        {
-            _selectedValueSelected.OnSelected += OnClick;
-            OnClick(_selectedValueSelected.CurrentValue);
-        }
-
-        public void ProduceUnit()
-        {
-            Instantiate(_unitPrefab,
-                new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)),
-                Quaternion.identity,
-                _unitsParent);
-        }
-
-        private void ONSelected(ISelectable selected)
-        {
-            _selection.gameObject.SetActive(selected != null);
-        }
-
-        private void OnClick(ISelectable selected)
-        {
-            if (selected != null)
-            {
-                ProduceUnit();
-            }
-
-            ONSelected(selected);
-
-        }
-    }
+    public override void ExecuteSpecificCommand(IProduceUnitCommand command) 
+        => Instantiate(command.UnitPrefab, 
+            new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), 
+            Quaternion.identity, 
+            _unitsParent);
 }
